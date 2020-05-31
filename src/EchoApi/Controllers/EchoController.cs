abfc034bcba86da2218731429ca6cfd28010aa15
Gary Lumsden <gjlumsden@gmail.com>
2020-05-31 16:41:46 +0100

    Initial commit.

---
+++ src/EchoApi/Controllers/EchoController.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EchoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EchoController : ControllerBase
+    {
+        [HttpGet("{value}")]
+        public EchoResponse Get(string value)
+        {
+            return new EchoResponse
+            {
+                EchoValue = value
+            };
+        }
+
+        [HttpPost]
+        public EchoResponse Post([FromBody] string value)
+        {
+            return new EchoResponse
+            {
+                EchoValue = value
+            };
+        }
+    }
+
+    public class EchoResponse
+    {
+        public string EchoValue { get; set; }
+        public string OsInformation => RuntimeInformation.OSDescription;
+        public string OsArchitecture => RuntimeInformation.OSArchitecture.ToString();
+    }
+}
---
+++ src/EchoApi/arm32.Dockerfile
@@ -0,0 +1,18 @@
+﻿FROM mcr.microsoft.com/dotnet/core/sdk:3.1 AS build
+WORKDIR /source
+
+# copy csproj and restore as distinct layers
+COPY *.csproj .
+RUN dotnet restore -r linux-arm
+
+# copy and publish app and libraries
+COPY . .
+RUN dotnet publish -c release -o /app -r linux-arm
+
+# final stage/image
+FROM mcr.microsoft.com/dotnet/core/aspnet:3.1-buster-slim-arm32v7
+WORKDIR /app
+EXPOSE 80
+ENV ASPNETCORE_URLS http://*:80
+COPY --from=build /app .
+ENTRYPOINT ["./EchoApi"]
